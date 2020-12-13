4961edb6ef5c3691ba77e674e0f6c1619bc92327
Omar <omar841203@gmail.com>
2020-12-12 21:37:23 -0600

    Login

--- CineSaltoDeFe/CineSaltoDeFe/Controllers/LoginController.cs
+++ CineSaltoDeFe/CineSaltoDeFe/Controllers/LoginController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.SqlClient;
+using CineSaltoDeFe.Models;
 namespace CineSaltoDeFe.Controllers
 {
     public class LoginController : Controller
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
         // GET: Login
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Login()
         {
             return View();
         }
+
+        void connectionString()
+        {
+            con.ConnectionString = " data source =DESKTOP-GR3RPN9; database = cine; integrated segurity = SSPI ";        }
+        public ActionResult Verificar( Login log)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select * from Users where username ='"+log.Nombre+ "'and password ='" + log.contrasena+ "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View();
+            }
+            else
+            {
+                con.Close();
+                return View();
+            }
+        }
+
+
     }
 }
